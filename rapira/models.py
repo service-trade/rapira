cedd05dbc0153e77cca91f1cca70ac4d22d5aae6
service-trade <litvinov@service-trade.ru>
2013-11-19 16:01:07 +1100

    Базовый вариант Wiki из примера в доке к Pyrmid.

--- rapira/models.py
+++ rapira/models.py
@@ -1,14 +1,29 @@
+# -*- coding: utf-8 -*-
+from persistent import Persistent
 from persistent.mapping import PersistentMapping
 
 
-class MyModel(PersistentMapping):
+class WikiStore(PersistentMapping):
     __parent__ = __name__ = None
 
 
+class WikiPage(Persistent):
+    def __init__(self, data):
+        self.data = data
+
+
 def appmaker(zodb_root):
-    if not 'app_root' in zodb_root:
-        app_root = MyModel()
-        zodb_root['app_root'] = app_root
+    if 'app_root' in zodb_root:
+        zodb_root['wiki_root'] = zodb_root.pop('app_root')
+
+    if not 'wiki_root' in zodb_root:
+        app_root = WikiStore()
+        frontpage = WikiPage(u'Это начальная страница.')
+        app_root['FrontPage'] = frontpage
+        frontpage.__name__ = 'FrontPage'
+        frontpage.__parent__ = app_root
+        zodb_root['wiki_root'] = app_root
         import transaction
         transaction.commit()
-    return zodb_root['app_root']
+
+    return zodb_root['wiki_root']
--- rapira/views.py
+++ rapira/views.py
@@ -1,7 +1,60 @@
+from docutils.core import publish_parts
+import re
+
+from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
-from .models import MyModel
 
+from .models import WikiPage
 
-@view_config(context=MyModel, renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'rapira'}
+# regular expression used to find WikiWords
+wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
+
+@view_config(context='.models.WikiStore')
+def view_wiki(context, request):
+    return HTTPFound(location=request.resource_url(context, 'FrontPage'))
+
+@view_config(context='.models.WikiPage', renderer='templates/view.pt')
+def view_wiki_page(context, request):
+    wiki = context.__parent__
+
+    def check(match):
+        word = match.group(1)
+        if word in wiki:
+            page = wiki[word]
+            view_url = request.resource_url(page)
+            return '<a href="%s">%s</a>' % (view_url, word)
+        else:
+            add_url = request.application_url + '/add_page/' + word 
+            return '<a href="%s">%s</a>' % (add_url, word)
+
+    content = publish_parts(context.data, writer_name='html')['html_body']
+    content = wikiwords.sub(check, content)
+    edit_url = request.resource_url(context, 'edit_page')
+    return dict(page = context, content = content, edit_url = edit_url)
+
+@view_config(name='add_page', context='.models.WikiStore',
+             renderer='templates/edit.pt')
+def add_page(context, request):
+    pagename = request.subpath[0]
+    if 'form.submitted' in request.params:
+        body = request.params['body']
+        page = WikiPage(body)
+        page.__name__ = pagename
+        page.__parent__ = context
+        context[pagename] = page
+        return HTTPFound(location = request.resource_url(page))
+    save_url = request.resource_url(context, 'add_page', pagename)
+    page = WikiPage('')
+    page.__name__ = pagename
+    page.__parent__ = context
+    return dict(page = page, save_url = save_url)
+
+@view_config(name='edit_page', context='.models.WikiPage',
+             renderer='templates/edit.pt')
+def edit_page(context, request):
+    if 'form.submitted' in request.params:
+        context.data = request.params['body']
+        return HTTPFound(location = request.resource_url(context))
+
+    return dict(page=context,
+                save_url=request.resource_url(context, 'edit_page'))
